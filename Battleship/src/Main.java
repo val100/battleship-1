b598d593ab41c6951995445115c63d7621fcdcb4
lgrohn <lgrohn@cc.helsinki.fi>
2013-05-16 21:43:32 +0300

    lisää luokkia

--- Battleship/src/Main.java
+++ Battleship/src/Main.java
@@ -1,5 +1,6 @@
 
 import battleship.domain.Board;
+import battleship.domain.TargetBoard;
 
 /*
  * To change this template, choose Tools | Templates
@@ -14,14 +15,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Battleship\n");
         Board board = new Board(5,5);
+        TargetBoard target = new TargetBoard(5,5);
         board.initBoard();
+        target.initBoard();
         board.drawBoard();
+        target.drawBoard();
         board.placeShip(1, 1, 1, 2);
+        target.placeShip(1, 1, 1, 2);
         board.drawBoard();
-        board.placeShip(3, 2, 0, 2);
-        //board.shoot(5, 2);
-        //board.shoot(5,4);
-        //board.drawBoard();
+        target.drawBoard();
+        //board.placeShip(3, 2, 0, 2);
+        board.shoot(2, 2);
+        board.shoot(1,1);
+        target.shoot(3, 0);
+        target.shoot(1,1);
+        board.drawBoard();
+        target.drawBoard();
         int[][] lauta = board.getBoard();
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
---
+++ Battleship/src/battleship/domain/TargetBoard.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship.domain;
+
+import static battleship.domain.Board.BOARD_BLANK;
+
+/**
+ *
+ * @author larg
+ */
+public class TargetBoard extends Board {
+    
+    public TargetBoard(int height, int width) {        
+        super(height, width);        
+    }
+    
+    @Override
+    public void drawBoard() {
+        for (int i = 0; i < super.getHeight(); i++) {
+            for (int j = 0; j < super.getWidth(); j++) {
+                if (super.getBoard()[i][j] == BOARD_BLANK) {
+                    System.out.print(".");
+                } else if (super.getBoard()[i][j] == BOARD_SHIP) {
+                    System.out.print(".");
+                } else if (super.getBoard()[i][j] == BOARD_HIT) {
+                    System.out.print("X");
+                } else if (super.getBoard()[i][j] == BOARD_MISS) {
+                    System.out.print("O");
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.println("");
+    }
+}
